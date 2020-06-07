6aea5470bfbc7b2d0d496afbd7343fba707cceff
IvayloBorisov <kadroki@abv.bg>
2020-06-07 14:49:29 +0300

    Updates

---
+++ precache-manifest.197a05026f0df32016a9acf7961a74f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3a886c9327fdff77078a383b873330",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "c0680c50b9302e49c485",
+    "url": "/covid-19-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652b503437b3be4cdb2d",
+    "url": "/covid-19-tracker/static/js/2.f966371f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker/static/js/2.f966371f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0680c50b9302e49c485",
+    "url": "/covid-19-tracker/static/js/main.140be7fc.chunk.js"
+  },
+  {
+    "revision": "fe7bce7a7b65f398812b",
+    "url": "/covid-19-tracker/static/js/runtime-main.df93df52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
